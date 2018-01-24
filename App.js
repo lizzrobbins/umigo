87511627c9f4287a2e0ae2f265a0d072d38e55fb
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2018-01-24 09:58:43 -0700

    added util dependency

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput, Navigator } from 'react-native';
+import Login from './screens/Login.js';
+
+export default class App extends React.Component {
+  render() {
+    return <Login />
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
