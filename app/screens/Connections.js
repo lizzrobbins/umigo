07c9dbe35c3ae9be6a9af84386e4cc8853cf9ee6
Elizabeth Robbins <EROBBINS1479@GMAIL.COM>
2018-01-28 18:16:52 -0700

    flushing out the connections page

--- app/screens/Connections.js
+++ app/screens/Connections.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View, TextInput, Navigator, ScrollView } from 'react-native';
-import { Button, Header, List, ListItem } from 'react-native-elements'
+import { Avatar, Button, Header, List, ListItem } from 'react-native-elements'
 
 export default class Connections extends React.Component {
   constructor(props){
@@ -21,36 +21,87 @@ export default class Connections extends React.Component {
     return (
       <View>
 
-          <Header
-            style={styles.header}
-            backgroundColor='#483954'
-            leftComponent={{ icon: 'settings', color: '#fff' }}
-            centerComponent={{ text: 'UmiGo', style: { color: '#fff', fontWeight: 'bold', fontSize: 20 },
-            onPress: this.props.gotoMainPage.bind(this),
-            }}
-            rightComponent={{ icon: 'people', color: '#fff',
-            // onPress: this.props.gotoConnections.bind(this),
-            }}
-          />
+        <Header
+          style={styles.header}
+          backgroundColor='#483954'
+          leftComponent={{ icon: 'settings', color: '#fff' }}
+          centerComponent={{ text: 'UmiGo', style: { color: '#fff', fontWeight: 'bold', fontSize: 20 },
+          onPress: this.props.gotoMainPage.bind(this),
+          }}
+          rightComponent={{ icon: 'people', color: '#fff',
+          // onPress: this.props.gotoConnections.bind(this),
+          }}
+        />
 
-        <List containerStyle={{marginBottom: 20}}>
-          {this.state.connections.map((userInfo, i) =>
-            <View>
-              <ListItem
-                roundAvatar
-                avatar={require('../../images/lizz.jpg')}
-                key={i}
-                title={userInfo.first_name}
-              />
+        {/* <Text
+          style={styles.pageTitle}>My Connections
+        </Text> */}
 
-              {/* <Image
-                style={styles.image}
-                source={require('../../images/lizz.jpg')}
-              /> */}
-              {/* <Text key={`photo1-${userInfo.id}`} style={styles.users}>{userInfo.photo1}</Text>
-              <Text key={`firstName-${userInfo.id}`} style={styles.users}>{userInfo.first_name}</Text> */}
-            </View>
-            )}
+        <List containerStyle={{marginTop: 0, marginBottom: 20, borderTopWidth: 0, borderColor: 'pink'}}>
+          <View key='0'>
+            <ListItem
+              avatar={<Image source={require(`../../images/lizz1.jpg`)}
+                style={{borderRadius:30, height:60, width:60 }} />}
+              title='Lizz'
+              subtitle='Denver, CO'
+            />
+          </View>
+          <View key='1'>
+            <ListItem
+              avatar={<Image source={require(`../../images/jake1.jpg`)}
+                style={{borderRadius:30, height:60, width:60 }} />}
+              title='Jake'
+              subtitle='Denver, CO'
+            />
+          </View>
+          <View key='2'>
+            <ListItem
+              avatar={<Image source={require(`../../images/kyle1.jpg`)}
+                style={{borderRadius:30, height:60, width:60 }} />}
+              title='Kyle'
+              subtitle='Denver, CO'
+            />
+          </View>
+          <View key='3'>
+            <ListItem
+              avatar={<Image source={require(`../../images/anna1.jpg`)}
+                style={{borderRadius:30, height:60, width:60 }} />}
+              title='Anna'
+              subtitle='Boulder, CO'
+            />
+          </View>
+          <View key='4'>
+            <ListItem
+              avatar={<Image source={require(`../../images/luke1.jpg`)}
+                style={{borderRadius:30, height:60, width:60 }} />}
+              title='Luke'
+              subtitle='Denver, CO'
+            />
+          </View>
+          <View key='5'>
+            <ListItem
+              avatar={<Image source={require(`../../images/ellen1.jpg`)}
+                style={{borderRadius:30, height:60, width:60 }} />}
+              title='Ellen'
+              subtitle='Lakewood, CO'
+            />
+          </View>
+          <View key='6'>
+            <ListItem
+              avatar={<Image source={require(`../../images/dakota1.jpg`)}
+                style={{borderRadius:30, height:60, width:60 }} />}
+              title='Dakota'
+              subtitle='Denver, CO'
+            />
+          </View>
+          <View key='7'>
+            <ListItem
+              avatar={<Image source={require(`../../images/meghan1.jpg`)}
+                style={{borderRadius:30, height:60, width:60 }} />}
+              title='Meghan'
+              subtitle='Boulder, CO'
+            />
+          </View>
         </List>
 
       </View>
@@ -58,8 +109,11 @@ export default class Connections extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  header: {
 
+const styles = StyleSheet.create({
+  pageTitle: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 30,
   }
 });
